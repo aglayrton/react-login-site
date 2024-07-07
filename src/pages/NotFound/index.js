e9e6f6d330ab579baef29f30c1404c52c41b8e3c
Ju <aglayrtonjuliao@gmail.com>
2024-07-07 16:42:06 -0300

    Página Not Found

---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,11 @@
+// pages/NotFound.js
+import React from "react";
+
+export const NotFound = () => {
+  return (
+    <div>
+      <h1>404 - Página Não Encontrada</h1>
+      <p>A página que você está procurando não existe.</p>
+    </div>
+  );
+};
