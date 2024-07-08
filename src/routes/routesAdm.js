34f7eaf42a604763ed887087826a4483ecfaf408
Ju <aglayrtonjuliao@gmail.com>
2024-07-08 14:14:52 -0300

    Ajuste do erro do retorno do route, que no caso tem que ser o filho sendo o Outlet

---
+++ src/routes/routesAdm.js
@@ -0,0 +1,29 @@
+import { Route, Routes, Navigate, useLocation, Outlet } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
+import { NotFound } from "../pages/NotFound";
+import { Login } from "../pages/Login";
+import { useContext } from "react";
+import { Context } from "../Context/AuthContext";
+
+function CustomRoute({ isPrivate }) {
+  const { authenticated } = useContext(Context);
+  const location = useLocation();
+
+  if (isPrivate && !authenticated) {
+    return <Navigate to='/' state={{ from: location }} />;
+  }
+
+  return <Outlet />;
+}
+
+export default function RoutesAdm() {
+  return (
+    <Routes>
+      <Route element={<CustomRoute isPrivate={true} />}>
+        <Route path='/dashboard/*' element={<Dashboard />} />
+      </Route>
+      <Route path='/' element={<Login />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  );
+}
