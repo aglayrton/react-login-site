517bea068b8252e7edc1492f469057972b48ccb3
Ju <aglayrtonjuliao@gmail.com>
2024-07-08 14:14:10 -0300

    Colacando o contexto e corrigindo erro de roteamento.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
+import { BrowserRouter, Router } from "react-router-dom";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Login } from "./pages/Login";
-import { NotFound } from "./pages/NotFound";
-import Dashboard from "./pages/Dashboard";
 import { AuthProvider } from "./Context/AuthContext";
-// Importando o componente Login
+import RoutesAdm from "./routes/routesAdm";
 
 function App() {
   return (
-    <div>
-      <AuthProvider>
-        <Router>
-          <Routes>
-            <Route path='/dashboard' element={<Dashboard />} />
-            <Route path='*' element={<NotFound />} />
-            <Route path='/' element={<Login />} />
-          </Routes>
-        </Router>
-      </AuthProvider>
-    </div>
+    <AuthProvider>
+      <BrowserRouter>
+        <RoutesAdm />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
