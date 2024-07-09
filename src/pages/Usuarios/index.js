424b3dfe46f57551df9c10b0722ba281ee3d3c8c
Ju <aglayrtonjuliao@gmail.com>
2024-07-09 13:38:34 -0300

    recuperando os dados da rota e exibindo

--- src/pages/Usuarios/index.js
+++ src/pages/Usuarios/index.js
@@ -1,3 +1,42 @@
+import { useEffect, useState } from "react";
+import { api } from "../../config/configApi";
+
 export function Usuarios() {
-  return <h1>Usuarios</h1>;
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const headers = {
+      headers: {
+        Authorization: "Bearen " + localStorage.getItem("token"),
+      },
+    };
+
+    await api
+      .get("/usuario", headers)
+      .then((response) => {
+        console.log(response.data.users);
+        setUsers(response.data.users);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <h1>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+            <p>{user.status}</p>
+          </div>
+        );
+      })}
+    </h1>
+  );
 }
